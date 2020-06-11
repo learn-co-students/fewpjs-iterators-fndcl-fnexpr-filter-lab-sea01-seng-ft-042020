bf8726611e0bef647c57921fd90b4cb0450ba521
github username <github email address>
2020-06-11 13:19:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, string) {
+  let result = array.filter(
+    (driver) => driver.toUpperCase() === string.toUpperCase()
+  );
+  return result;
+}
+
+function fuzzyMatch(array, string) {
+  let result = array.filter((driver) => {
+    if (driver.startsWith(string)) {
+      return true
+    }
+  });
+  return result; // array.filter will return the elements that it gets a true result from, since i'm only returning true for the elements at the beginning of only those elements will return in  new array
+}
+
+function matchName(arrayOfObjs,string){
+ return arrayOfObjs.filter(driver => driver.name === string)
+}
